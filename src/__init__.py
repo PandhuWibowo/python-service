ed5fa0f441a00054c85954119a1f52c929be7715
Pandhu Wibowo <pandhu.wibowo@bvarta.com>
2024-12-26 22:31:08 +0700

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -9,18 +9,11 @@ def get_root_message():
 def calculate_addition(num1: int, num2: int):
     return {"result": num1 + num2}
 
-def calculate_substract(num1: int, num2: int):
-    return {"result": num1 - num2}
-
 # FastAPI routes
 @app.get("/")
 async def root():
     return get_root_message()
 
 @app.get("/addition/{num1}/{num2}")
-async def addition(num1: int, num2: int):
-    return calculate_addition(num1, num2)
-
-@app.get("/substract/{num1}/{num2}")
 async def addition(num1: int, num2: int):
     return calculate_addition(num1, num2)
